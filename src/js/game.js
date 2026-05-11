bdfbbdeb78e2fe4f50832efafdfffeb183d111dd
robinm50 <robin.moerkens@icloud.com>
2026-05-11 15:54:06 +0200

    add shark

--- src/js/game.js
+++ src/js/game.js
@@ -17,15 +17,26 @@ export class Game extends Engine {
     startGame() {
         console.log("start de game!")
         const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+        fish.graphics.use(Resources.Fish.toSprite()) // plaatje toevoegen aan je actor
+        fish.pos = new Vector(500, 300)  // positie van je actor
+        fish.vel = new Vector(-10,0) // velociteit van je actor 
+        fish.events.on("exitviewport", (e) => this.fishLeft(e)) //wat er gebeurt als de actor het scherm verlaat na de , is wat die uitvoert
+        this.add(fish)// toevoegen aan de scene this omdat je in de game zit
+
+        const shark = new Actor()
+        shark.graphics.use(Resources.Shark.toSprite()) 
+        shark.pos= new Vector(1000, 500)
+        shark.vel = new Vector(300,0)
+        shark.events.on("exitviewport", (e) => this.sharkRight(e))
+        this.add(shark)
     }
+
 
     fishLeft(e) {
         e.target.pos = new Vector(1350, 300)
+    }
+    sharkRight(e) {     
+           e.target.pos = new Vector(-50, 500)
     }
 }
 
